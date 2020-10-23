2f0d76eff80fd5feb43bd71852871e4256c50a6c
Arthur <arthuromp000@gmail.com>
2020-10-23 20:22:38 -0300

    Add code for tests

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -16,26 +16,42 @@ class ProductsRepository implements IProductsRepository {
     this.ormRepository = getRepository(Product);
   }
 
-  public async create({
-    name,
-    price,
-    quantity,
-  }: ICreateProductDTO): Promise<Product> {
-    // TODO
+  public async create({name, price, quantity}: ICreateProductDTO): Promise<Product> {
+    
+    const product = await this.ormRepository.create({ name, price, quantity });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    
+    const product = await this.ormRepository.findOne({
+      where: {
+        name
+      }
+    });
+
+    return product;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    
+    const productsIds = products.map(product => product.id);
+
+    const productsList = await this.ormRepository.find({
+      where: {
+        id: In(productsIds)
+      }
+    });
+
+    return productsList;
   }
 
-  public async updateQuantity(
-    products: IUpdateProductsQuantityDTO[],
-  ): Promise<Product[]> {
-    // TODO
+  public async updateQuantity(products: IUpdateProductsQuantityDTO[]): Promise<Product[]> {
+    
+    return this.ormRepository.save(products);
   }
 }
 
